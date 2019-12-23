06f97d7fadac3c3a251e325750fe64a5df39e35a
WasAmBe <wasambe@gmail.com>
2019-12-22 22:02:53 -0500

    dsf

--- test.js
+++
@@ -1,7 +0,0 @@
-var globalvarjs="globalvarjs";
-function testjs(){
-alert("This is function testjs from the test.js page"); 
-//var localvarjs="localvarjs";
-alert("I am displaying variable globalvarjs" + globalvarjs);
-}
-
