edf520f2bb894fb6d4e92571f6ab25a5597b843c
WasAmBe <wasambe@gmail.com>
2019-12-26 15:54:01 -0500

    111

--- adminSqlJson.php
+++ adminSqlJson.php
@@ -27,17 +27,7 @@ else{
 	  }
 }
 
-//  echo json_encode($data, JSON_PRETTY_PRINT);
-//it will create file results.json with writing mode.
-//you can read more about file handling in PHP here. 
-//$fp = fopen('results.json', 'w');
-//json_enconde($array, $options(optional) is the method to convert array into JSON
-//$myObj = fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
-//close the file
-//fclose($fp);
-
 $myObj = json_encode($data);
-//$myObj = readfile("results.json");
 echo $myObj;
-//echo $data;
+
 ?>
